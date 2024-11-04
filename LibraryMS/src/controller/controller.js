d9ad578614088518cb1f5380f0258f663c80b15a
krish <krishcode9@gmail.com>
2024-11-04 23:51:08 +0530

    student list UI done

--- LibraryMS/src/controller/controller.js
+++ LibraryMS/src/controller/controller.js
@@ -8,11 +8,20 @@ const addStudentController = function(data){
 }
 
 
-
+// add new student
 const addStudentButton = document.querySelector('.add-student-btn');
 addStudentButton.addEventListener('click',(e) =>{
+  document.querySelector('#student-list').innerHTML='';
   // render UI
   AddStudentView.render()
   // Attach the form submission handler
   AddStudentView.insertNewStudentHandler(addStudentController)
 })
+
+//show student list
+const studentList =  document.querySelector('#view-student-list');
+studentList.addEventListener('click',function(){
+  AddStudentView.listMarkup(model.state.students)
+})
+//click on book to check how many students take books
+const allStudentsList=document.querySelector('#student-list');
--- LibraryMS/src/views/addStudentView.js
+++ LibraryMS/src/views/addStudentView.js
@@ -2,11 +2,9 @@ import View from "./View";
 
 class AddStudentView extends View{
   _parentElement = document.getElementById('add-student');
-
-  
-
+  _list = document.getElementById('student-list');
   insertNewStudentHandler(addStudentController){
-    this._parentElement.querySelector('#student-form').addEventListener('submit',function(e){
+    this._parentElement.querySelector('#student-form').addEventListener('submit',(e) =>{
       e.preventDefault();
       //console.log(e.target)//= form
       const data = new FormData(e.target);
@@ -14,8 +12,13 @@ class AddStudentView extends View{
       for(let [key,value] of data){
         studentObj[key]=value
       }
-      studentObj.id='STU'+(Math.floor(Math.random()*100)+1)
+      studentObj.id='STU'+(Math.floor(Math.random()*100)+1);
+      studentObj.maxAllowedBook=10;
+      studentObj.assignBooks=[];
       addStudentController(studentObj)
+      this._data=studentObj;
+      this.resetInputField();
+
     })
   }
 
@@ -42,6 +45,43 @@ class AddStudentView extends View{
         <button class="btn">Add New Student</button>
       </form>
   `
+  }
+
+  resetInputField(){
+    this._parentElement.querySelector('#name').value='';
+    this._parentElement.querySelector('#class').value='';
+    this._parentElement.querySelector('#phone').value='';
+    this._parentElement.querySelector('#address').value='';
+  }
+
+  listMarkup(data){
+    if(data.length === 0) return null;
+    const markup=` <h2>Students List</h2>      
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>StudentId</th>
+          <th>Phone</th>
+          <th>Address</th>
+          <th>Class</th>
+          <th>Assigned Books</th>
+        </tr>
+       ${data.map(std => {
+          return `<tr class="student-record">
+          <td>${std.name}</td>
+          <td>${std.id}</td>
+          <td>${std.phone}</td>
+          <td>${std.address}</td>
+          <td>${std.class}</td>
+          <td class="asigned-book">Books</td>
+          </tr>`
+        })}
+      </table>`
+    this._parentElement.innerHTML='';
+    document.querySelector('#student-list').innerHTML=markup;
+  }
+  showBookAssinedPerStudent(){
+
   }
 }
 
